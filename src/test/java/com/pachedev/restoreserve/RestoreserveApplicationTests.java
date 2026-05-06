0dd5a457cdc0962b0a64a10f6480f41a9a82094b
dani <cloudxdam@gmail.com>
2026-05-06 08:34:58 +0100

    initial commmit

---
+++ src/test/java/com/pachedev/restoreserve/RestoreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pachedev.restoreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoreserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
